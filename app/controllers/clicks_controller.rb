b02601d635ff479652a334a6a9fcabadd5a70ebf
EvNomad <evilnomadx@gmail.com>
2014-11-14 13:06:15 +0200

    private functions for getting scoped clicks were added

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -1,17 +1,34 @@
 class ClicksController < ApplicationController
 
   def index
-    #@clicks = Click.all
-    @offers = Offer.all
+
+    if params[:month_scope].nil?
+      get_clicks
+    else
+      get_clicks_by_scope(params[:month_scope].to_i.months.ago)
+    end
+
   end
 
 
   def show
 
     @offer = Offer.find_by_token(params[:token])
-    Click.create!(:offer_id => @offer.id, :ip => request.remote_ip)
+    Click.create!(:offer_id => @offer.id, :ip => request.remote_ip, :traffic_source => "localhost")
 
     redirect_to :back
 
+  end
+
+  private
+
+  def get_clicks_by_scope(scope)
+    @offers = Offer.by_clicks_created_between(scope, Time.now)
+    @sources = Click.created_between(scope, Time.now).group_by(&:traffic_source)
+  end
+
+  def get_clicks
+    @offers = Offer.all
+    @sources = Click.all.group_by(&:traffic_source)
   end
 end
