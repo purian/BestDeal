9b6b6ab074931b4b8ed37ee283df2181661e1b4b
EvNomad <evilnomadx@gmail.com>
2014-11-14 13:07:03 +0200

    helper for getting the amount of statistics days added

--- app/helpers/clicks_helper.rb
+++ app/helpers/clicks_helper.rb
@@ -1,2 +1,9 @@
 module ClicksHelper
+  def statistics_days(month_scope)
+    if month_scope
+      "Last #{month_scope.to_i * 30} days statistics."
+    else
+      'All clicks.'
+    end
+  end
 end
