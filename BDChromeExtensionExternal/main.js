f61a956e75115aeb6c6a0aa22905fed015b749b6
EvNomad <evilnomadx@gmail.com>
2014-11-18 11:24:05 +0200

    port on localhost changed to 3000

--- BDChromeExtensionExternal/main.js
+++ BDChromeExtensionExternal/main.js
@@ -2,7 +2,7 @@
 // prod_url = https://blooming-cliffs-1855.herokuapp.com
 // dev_url = http://localhost:3000
 (function() {
-	var url = 'http://localhost:3001'
+	var url = 'http://localhost:3000'
 	$('head').append("<script type='text/javascript' src='"+url+"/main.js'>");
 	$('head').append("<link rel='stylesheet' type='text/css' href='"+url+"/main.css'>");
 })();
